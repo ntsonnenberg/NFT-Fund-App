4f879989548a964cccab95b921fa049def7ba8f1
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2022-02-09 18:11:37 -0700

    created environment variable for node listening port

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const app = require('./server');
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 });
