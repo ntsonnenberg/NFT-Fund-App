7cff5d596ed3adb81355f665052ab5a80e5c933f
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2022-02-20 14:25:10 -0800

    configured docker containers for postgres and set up environment variables to be used in API server

---
+++ api/database/account.js
@@ -0,0 +1,77 @@
+const bcrypt = require("bcryptjs");
+const { set } = require("core-js/core/dict");
+const { user } = require("pg/lib/defaults");
+const uuid = require("uuid").v4;
+
+exports.createAccount = async function (client, username, password, isManager) {
+  const accountId = uuid();
+  const salt = await bcrypt.genSalt(10);
+
+  const { rowCount } = await client.query({
+    name: "create-account",
+    text: "INSERT INTO accounts (accountId, username, password, isManager) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING",
+    values: [accountId, username, await bcrypt.hash(password, salt), isManager],
+  });
+
+  return rowCount > 0 ? accountId : undefined;
+};
+
+exports.getAccount = async function (client, accountId) {
+  const { rows } = await client.query({
+    name: "get-acount-by-id",
+    text: "SELECT * FROM accounts WHERE account_id=$1",
+    values: [accountId],
+  });
+
+  return rows[0];
+};
+
+exports.updateAccount = async function (client, accountId, data) {
+  const { username, password, isManager } = data;
+  const values = [];
+  const sets = [];
+
+  if (username !== undefined) {
+    values.push(username);
+    sets.push("username=$" + values.length);
+  }
+
+  if (password !== undefined) {
+    values.push(password);
+    sets.push("password=$" + values.length);
+  }
+
+  if (isManager !== undefined) {
+    values.push(isManager);
+    sets.push("isManager=$" + values.length);
+  }
+
+  if (values.length === 0) {
+    return await exports.getAccount(client, accountId);
+  }
+
+  values.push(accountId);
+
+  const { rows } = client.query({
+    name: "update-account",
+    text:
+      "UPDATE accounts SET  " +
+      sets.join(", ") +
+      " WHERE account_id=$" +
+      values.length +
+      " RETURNING *",
+    values,
+  });
+
+  return rows[0];
+};
+
+exports.deleteAccount = async function (client, accountId) {
+  const { rowCount } = client.query({
+    name: "delete-account",
+    text: "DELETE FROM accounts WHERE account_id=$1",
+    values: [accountId],
+  });
+
+  return rowCount > 0;
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+
 const app = require("./server");
 
-const port = process.env.PORT || 3001;
+//const port = process.env.PORT || 3001;
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
