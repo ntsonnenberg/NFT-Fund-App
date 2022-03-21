7a4813f0b599138444099325f57f43c87d2e3234
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2022-03-21 15:41:19 -0600

    implemented a vue component for a fund card

---
+++ components/FundCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <v-card>
+          <p>I am a fund</p>
+          <input v-model="fundName" placeholder="enter fund name" />
+          <button @click="$emit('view', fundName)">View Name</button>
+      </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FundCardComponent',
+
+    emits: ['view'],
+
+    data() {
+        return {
+            fundName: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,31 @@
 <div>
   <NuxtLink to="/login">Login</NuxtLink>
   <h1>NFT Fund</h1>
+  <fund-card @view="showFundName"/>
 </div>
   
 </template>
 
 <script>
+  import FundCard from '@/components/FundCard.vue'
+
   export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+
+    data() {
+      return {
+        fundName: '' 
+      }
+    },
+
+    components: {
+      FundCard
+    },
+
+    methods: {
+      showFundName (event) {
+        alert("This fund is named: " + event);
+      }
+    }
   }
 </script>
